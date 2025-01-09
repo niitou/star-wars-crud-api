f7c5a1b2ca9fe1d647ddd89c4d1d0436d16ed07a
noel <noelnolife@gmail.com>
2025-01-09 12:22:20 +0700

    adding entities

--- src/characters/entities/character.entity.ts
+++ src/characters/entities/character.entity.ts
@@ -1,1 +1,59 @@
-export class Character {}
+import { ID, ObjectType, Field } from '@nestjs/graphql';
+import { Planet } from 'src/planets/entities/planet.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  OneToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+export enum CharacterGender {
+  MALE = 'Male',
+  FEMALE = 'Female',
+}
+
+@Entity()
+@ObjectType()
+export class Character {
+  @Field(() => ID)
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Field()
+  @Column({ nullable: false })
+  name: string;
+
+  @Field()
+  @Column()
+  species: string;
+
+  @Field()
+  @Column({ type: 'enum', enum: CharacterGender })
+  gender;
+
+  @Field()
+  @Column({ type: 'float' })
+  height;
+
+  @Field()
+  @Column()
+  weight: number;
+
+  @Field()
+  @OneToOne(() => Planet)
+  @JoinColumn()
+  planet: Planet;
+
+  @Field()
+  @Column({ type: 'text' })
+  description;
+
+  @CreateDateColumn()
+  created_at;
+
+  @UpdateDateColumn()
+  updated_at;
+}
--- src/planets/entities/planet.entity.ts
+++ src/planets/entities/planet.entity.ts
@@ -1,1 +1,38 @@
-export class Planet {}
+import { Field, ID, ObjectType } from '@nestjs/graphql';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+@ObjectType()
+export class Planet {
+  @Field(() => ID)
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Field()
+  @Column({ nullable: false })
+  name: string;
+
+  @Field()
+  @Column()
+  diameter: number;
+
+  @Field()
+  @Column()
+  climate: string;
+
+  @Field()
+  @Column('varchar', { array: true })
+  terrain: string[];
+
+  @CreateDateColumn()
+  created_at;
+
+  @UpdateDateColumn()
+  updated_at;
+}
