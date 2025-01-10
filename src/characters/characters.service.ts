9af425860ba862c07dbbc9a2a400f2233dda6420
noel <noelnolife@gmail.com>
2025-01-10 12:58:53 +0700

    adding relation

--- src/characters/characters.service.ts
+++ src/characters/characters.service.ts
@@ -10,34 +10,34 @@ export class CharactersService {
   constructor(
     @InjectRepository(Character)
     private characterRepository: Repository<Character>,
-  ) {}
+  ) { }
   async create(createcharacterInput: CreateCharacterInput) {
-      const characterData = await this.characterRepository.save(createcharacterInput);
-      return this.characterRepository.save(characterData)
-    }
-  
-   async findAll() {
-      return await this.characterRepository.find();
-    }
-  
-    async findOne(id: number) {
-      const characterData = await this.characterRepository.findOneBy({ id });
-      if(!characterData) throw new HttpException('Character not found', 404)
-      return characterData
-    }
-  
-    async update(id: number, updatecharacterInput: UpdateCharacterInput) {
-      const existingcharacter = await this.characterRepository.findOneBy({id})
-      if(!existingcharacter) throw new HttpException('Character not found', 404)
-  
-      const characterData = this.characterRepository.merge(existingcharacter, updatecharacterInput)
-      return await this.characterRepository.save(characterData);
-    }
-  
-    async remove(id: number) {
-      const characterData = await this.characterRepository.findOneBy({id})
-      if(!characterData) throw new HttpException('Character not found', 404)
-  
-      return await this.characterRepository.remove(characterData);
-    }
+    const characterData = await this.characterRepository.create(createcharacterInput);
+    return this.characterRepository.save(characterData)
+  }
+
+  async findAll() {
+    return await this.characterRepository.find({ relations: ['homeworld'] });
+  }
+
+  async findOne(id: number) {
+    const characterData = await this.characterRepository.findOne({ where: { id: id }, relations: ['homeworld'] });
+    if (!characterData) throw new HttpException('Character not found', 404)
+    return characterData
+  }
+
+  async update(id: number, updatecharacterInput: UpdateCharacterInput) {
+    const existingcharacter = await this.characterRepository.findOneBy({ id })
+    if (!existingcharacter) throw new HttpException('Character not found', 404)
+
+    const characterData = this.characterRepository.merge(existingcharacter, updatecharacterInput)
+    return await this.characterRepository.save(characterData);
+  }
+
+  async remove(id: number) {
+    const characterData = await this.characterRepository.findOneBy({ id })
+    if (!characterData) throw new HttpException('Character not found', 404)
+
+    return await this.characterRepository.remove(characterData);
+  }
 }
