0b972ccc8706eca5cbe18c36625444c8c21ae338
noel <noelnolife@gmail.com>
2025-01-10 12:57:58 +0700

    import entity

--- src/characters/characters.module.ts
+++ src/characters/characters.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CharactersService } from './characters.service';
 import { CharactersResolver } from './characters.resolver';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Character } from './entities/character.entity';
 
 @Module({
+  imports : [TypeOrmModule.forFeature([Character])],
   providers: [CharactersResolver, CharactersService],
 })
 export class CharactersModule {}
--- src/planets/planets.module.ts
+++ src/planets/planets.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PlanetsService } from './planets.service';
 import { PlanetsResolver } from './planets.resolver';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Planet } from './entities/planet.entity';
 
 @Module({
+  imports : [TypeOrmModule.forFeature([Planet])],
   providers: [PlanetsResolver, PlanetsService],
 })
 export class PlanetsModule {}
